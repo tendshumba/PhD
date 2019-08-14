ede380de501343e10530123de01d28ea9be29dcc
tendshumba <tendshumba@yahoo.com>
2019-08-14 14:21:13 +0200

    Update prod2.m

--- prod2.m
+++ prod2.m
@@ -1,5 +1,5 @@
 prod2:=function(u,v)
-if u or v not in V then
+if not ( u in V) or  not( v in V) then
   print "error, vector must be in V";
 else
   return &+[u[Floor(i/1782)+1]*v[i-Floor(i/1782)]*prods1[i]:i in [1..#prods1]];
